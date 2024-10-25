d8a888bb65c91dd351723f7d4a030f4733c64c87
Luis <luishidalgoa@outlook.es>
2024-10-25 17:17:24 +0200

    Students

---
+++ microservice-student/src/test/java/dev/luishidalgoa/student/MicroserviceStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luishidalgoa.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
