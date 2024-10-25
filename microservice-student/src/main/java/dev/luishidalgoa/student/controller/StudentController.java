ea9101a34210b06574b0ebd953acc9cb6ba2d74c
Luis <luishidalgoa@outlook.es>
2024-10-25 23:50:39 +0200

    se ha creado un DTO para abstraer la información del microservicio Student dentro del microservicio Course. Ademas se ha creado una clase en course encargada de consultar con http a la api de Stundent y por ultimo se ha habilitado un endpoint en Course para hacer la consulta entre las 2 apis

--- microservice-student/src/main/java/dev/luishidalgoa/student/controller/StudentController.java
+++ microservice-student/src/main/java/dev/luishidalgoa/student/controller/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/api/student")
 public class StudentController {
     @Autowired
     private IStudentService studentService;
