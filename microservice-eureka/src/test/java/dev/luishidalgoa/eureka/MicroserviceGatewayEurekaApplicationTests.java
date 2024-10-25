8d85d3d1f60b47810dbb1aedc91c9bc53d56b49e
Luis <luishidalgoa@outlook.es>
2024-10-25 16:51:48 +0200

    eureka project

---
+++ microservice-eureka/src/test/java/dev/luishidalgoa/eureka/MicroserviceGatewayEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luishidalgoa.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGatewayEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
